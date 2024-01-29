c95333803f7b542d7c054305e7727c80f782815e
Akhil Sai Ram <akhilsairammaruti@gmail.com>
2024-01-29 11:13:41 +0530

    feat: add states for hard drop and soft movement

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ function setupBoard(rows: number, columns: number) {
   for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
     for (let colIndex = 0; colIndex < columns; colIndex++) {
       const gridCell = document.createElement('div')
-      gridCell.className = 'grid-cell cellIndex-' + colIndex + '-' + rowIndex;
+      gridCell.className = 'grid-cell col-' + colIndex + ' row-' + rowIndex;
       gridEl.appendChild(gridCell)
     }
   }
@@ -26,22 +26,56 @@ function displayPiece(piece: TPiece, position: [number, number]) {
   piece.forEach(block => {
     const x = block[0] + position[0]
     const y = block[1] + position[1]
-    const el = document.querySelector('.cellIndex-' + x + '-' + y);
+    const el = document.querySelector('.col-' + x + '.row-' + y);
     el?.classList.add('grid-cell-active');
   })
 }
 
-function runGameLoop(send: (type: 'MOVE_DOWN') => void) {
-  setInterval(() => send('MOVE_DOWN'), 500)
+function runGameLoop(send: (type: 'MOVE_DOWN_FROM_ENGINE') => void) {
+  setInterval(() => send('MOVE_DOWN_FROM_ENGINE'), 500)
 }
 
 function setupGame() {
   const { start, state, send, subscribe } = interpret(tetrisMachine);
-  const play = setUpDummyButton('play')
+  let prevAccelerate = state.context.shouldAccelerate;
+  const play = setUpDummyButton('play');
   play.onclick = () => send('PLAY');
   document.body.appendChild(play);
+  const scoreEl = document.getElementById('score')
   setupBoard(state.context.gameDims.rows, state.context.gameDims.columns);
   subscribe(newState => displayPiece(newState.context.piece, newState.context.position))
+  subscribe(newState => {
+    play.innerText = newState.matchesAny('paused') ? 'Play' : 'Pause';
+    play.onclick = () => send(newState.matchesAny('paused') ? 'PLAY' : 'PAUSE')
+  })
+  subscribe(newState => {
+    const { score } = newState.context;
+    if (!scoreEl) {
+      return;
+    }
+    scoreEl.innerHTML = 'Score: ' + score
+  })
+
+  let timerId: number | undefined;
+  subscribe(newState => {
+    const { direction, shouldAccelerate } = newState.context;
+
+    if (prevAccelerate !== shouldAccelerate) {
+      prevAccelerate = shouldAccelerate;
+      if (!shouldAccelerate) {
+        clearInterval(timerId);
+      }
+      if (shouldAccelerate) {
+        const actionBasedOnDirection = direction === 'bottom' ? 'MOVE_DOWN' : direction === 'left' ? 'MOVE_LEFT' : direction === 'right' ? 'MOVE_RIGHT' : undefined;
+        if (actionBasedOnDirection) {
+          timerId = setInterval(() => {
+            send(actionBasedOnDirection)
+          }, 100);
+        }
+      }
+    }
+  })
+
   start();
   runGameLoop(send);
 }
--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,6 @@
 import { MachineConfig, createContext, createEvents, createStates } from "@simple-state-machine/core";
 import { TPiece } from "./types";
-import { activatePiece, checkForCollision, definePieceShapes, erasePiece, pickRandomPiece, rotatePiece, updateBoardState } from "./utils";
+import { activatePiece, checkForRowFill, createBoard, definePieceShapes, erasePiece, pickRandomPiece, rotatePiece, shiftFilledCells, updateBoardState, blinkPieces, reconstructBoard, removeBlinkPieces, animateGameComplete, returnFuturePositionOnHardDrop, setStartPosition } from "./actions";
 
 
 interface IContext {
@@ -12,25 +12,37 @@ interface IContext {
     },
     position: [number, number],
     futurePosition: [number, number],
-    direction: 'bottom' | 'left' | 'right'
+    futurePieceAfterRotation: TPiece,
+    direction: 'bottom' | 'left' | 'right' | 'setByDefault',
+    filledColIndexes: number[],
+    score: number,
+    shouldAccelerate: boolean
 }
 
 const pieces = definePieceShapes();
 
-const states = createStates('idle', 'playing', 'checkForCollision');
 
-const events = createEvents('ROTATE_PIECE', 'UPDATE_BOARD_STATE', 'MOVE_LEFT', 'MOVE_RIGHT', 'MOVE_DOWN', 'PLAY', 'COLLISION_DETECTED', 'COLLISION_UN_DETECTED', 'GENERATE_NEW_PIECE')
+const states = createStates('idle', 'playing', 'checkForCollision', 'checkForRowFill', 'filledAnimation', 'paused', 'gameCompleteAnimation');
+
+const events = createEvents('ROTATE_PIECE', 'UPDATE_BOARD_STATE', 'MOVE_LEFT', 'MOVE_RIGHT', 'MOVE_DOWN', 'MOVE_DOWN_FROM_ENGINE', 'PLAY', 'COLLISION_DETECTED', 'COLLISION_UN_DETECTED', 'GENERATE_NEW_PIECE', 'CHECK_FOR_ROW_FILL', 'UPDATE_FILLED_COL_INDEXES', "GAME_COMPLETE", 'PAUSE', 'HARD_DROP', 'DECELERATE');
+
+const randomPiece = pickRandomPiece(pieces)
+
 
 const context: IContext = createContext({
     boardState: [[]],
-    piece: pickRandomPiece(pieces),
-    position: [4, 0],
+    piece: randomPiece,
+    futurePieceAfterRotation: randomPiece,
+    position: [0,0],
     gameDims: {
         rows: 20,
         columns: 10
     },
-    futurePosition: [4, 0],
-    direction: 'bottom'
+    futurePosition: [0,0],
+    direction: 'bottom',
+    filledColIndexes: [],
+    score: 0,
+    shouldAccelerate: false
 })
 
 export const tetrisMachine = new MachineConfig<typeof states, IContext, typeof events>(states, context, events);
@@ -38,88 +50,222 @@ export const tetrisMachine = new MachineConfig<typeof states, IContext, typeof e
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { whenIn } = tetrisMachine;
 
+tetrisMachine.on('PAUSE').moveTo('paused');
+
+whenIn('paused').on('PLAY')
+    .moveTo('playing')
+
 whenIn('idle').invokeCallback((context, callback) => {
     const { gameDims } = context
     const { rows, columns } = gameDims;
-    const emptyRow = [...new Array(columns) as number[]].map(() => 0);
-    const emptyBoard = [...new Array(rows) as number[]].map(() => [...emptyRow]);
+    const emptyBoard = createBoard(rows, columns)
     callback({
         type: 'UPDATE_BOARD_STATE',
         data: emptyBoard
     })
-}).on('UPDATE_BOARD_STATE').updateContext({
-    boardState: (_, event) => event.data as number[][]
 })
+    .on('UPDATE_BOARD_STATE')
+    .moveTo('paused')
+    .updateContext({
+        boardState: (_, event) => event.data as number[][],
+        position: setStartPosition
+    })
+    .fireAndForget(reconstructBoard)
 
-whenIn('idle').on('PLAY').moveTo('playing')
+whenIn('idle')
+    .on('PLAY')
+    .moveTo('playing')
 
-whenIn('playing').on('ROTATE_PIECE').fireAndForget(context => {
-    erasePiece(context.piece, context.position)
-}).updateContext({
-    piece: (context) => context.piece === pieces['OPiece'] ? context.piece : rotatePiece(context.piece)
-}).fireAndForget(context => activatePiece(context.piece, context.position))
+whenIn('checkForCollision')
+    .invokeCallback((context, callback) => {
+        const { futurePosition, boardState, futurePieceAfterRotation } = context;
 
+        const didPieceTouchFinalCol = futurePieceAfterRotation.some(([x, y]) => boardState[y + futurePosition[1]] == undefined || boardState[y + futurePosition[1]][x + futurePosition[0]] === 1);
+        if (didPieceTouchFinalCol) {
+            const didPieceAlsoTouchOGPosition = futurePieceAfterRotation.some(([, y]) => y + futurePosition[1] === 1);
+            if (didPieceAlsoTouchOGPosition) {
+                callback('GAME_COMPLETE')
+            }
+            else
+                callback('CHECK_FOR_ROW_FILL');
+            return;
+        }
+        const didNotCollide = futurePieceAfterRotation.every(([x, y]) => boardState[futurePosition[1] + y][futurePosition[0] + x] === 0);
+        if (didNotCollide) {
+            callback('COLLISION_UN_DETECTED')
+        }
+        else {
+            callback('COLLISION_DETECTED')
+        }
+    })
 
-whenIn('checkForCollision').invokeCallback((context, callback) => {
-    const { piece, futurePosition, boardState, direction } = context;
-    const collided = checkForCollision(piece, futurePosition, boardState, direction);
-    if (collided === 'bottom') {
-        callback('GENERATE_NEW_PIECE')
-    }
-    else if (collided === 'side') {
-        callback('COLLISION_DETECTED');
+whenIn('checkForCollision')
+    .on('COLLISION_DETECTED')
+    .moveTo('playing')
+    .updateContext({
+        futurePieceAfterRotation: context => context.piece
+    })
+
+whenIn('checkForCollision')
+    .on('CHECK_FOR_ROW_FILL')
+    .moveTo('checkForRowFill')
+    .updateContext({
+        boardState: updateBoardState
+    })
+
+whenIn('checkForCollision')
+    .on('COLLISION_UN_DETECTED')
+    .moveTo('playing')
+    .fireAndForget(erasePiece)
+    .updateContext({
+        position: context => [...context.futurePosition],
+        piece: context => [...context.futurePieceAfterRotation]
+    })
+    .fireAndForget(activatePiece);
+
+whenIn('checkForCollision').on('GAME_COMPLETE')
+    .moveTo('gameCompleteAnimation')
+    .fireAndForget(animateGameComplete)
+
+whenIn('checkForRowFill').invokeCallback((context, callback) => {
+    const { boardState } = context;
+    const filledColIndexes = checkForRowFill(boardState);
+    if (filledColIndexes.length > 0) {
+        callback({
+            type: 'UPDATE_FILLED_COL_INDEXES',
+            data: {
+                filledColIndexes: filledColIndexes
+            }
+        })
     }
     else {
-        callback('COLLISION_UN_DETECTED')
+        callback('GENERATE_NEW_PIECE')
     }
 })
 
-whenIn('checkForCollision').on('COLLISION_DETECTED').moveTo('playing');
-whenIn('checkForCollision').on('GENERATE_NEW_PIECE').moveTo('playing').updateContext({
-    piece: () => pickRandomPiece(pieces),
-    position: [4, 0],
-    boardState: context => updateBoardState(context.boardState, context.piece, context.position)
-})
-whenIn('checkForCollision').on('COLLISION_UN_DETECTED').moveTo('playing').fireAndForget(context => erasePiece(context.piece, context.position)).updateContext({
-    position: context => [...context.futurePosition]
-}).fireAndForget(context => activatePiece(context.piece, context.position))
+whenIn('checkForRowFill').on('GENERATE_NEW_PIECE')
+    .moveTo('playing')
+    .updateContext({
+        piece: () => pickRandomPiece(pieces),
+        position: setStartPosition ,
+        futurePosition: setStartPosition,
+        direction: 'setByDefault',
+        shouldAccelerate: false
+    })
+    .updateContext({
+        futurePieceAfterRotation: context => context.piece
+    })
+
+whenIn('checkForRowFill').on('UPDATE_FILLED_COL_INDEXES')
+    .moveTo('filledAnimation')
+    .updateContext({
+        filledColIndexes: (_, event) => event.data.filledColIndexes as number[]
+    })
+    .fireAndForget(context => {
+        blinkPieces(context.filledColIndexes)
+    })
+
+whenIn('filledAnimation').after(1000)
+    .moveTo('playing')
+    .updateContext({
+        piece: () => pickRandomPiece(pieces),
+        position: setStartPosition,
+        shouldAccelerate: false
+    })
+    .fireAndForget(removeBlinkPieces)
+    .updateContext({
+        filledColIndexes: [],
+        boardState: context => shiftFilledCells(context.boardState, context.filledColIndexes, context.gameDims.columns),
+        score: context => context.score + context.filledColIndexes.length,
+        futurePieceAfterRotation: context => context.piece
+    })
+    .fireAndForget(reconstructBoard)
+
 
 whenIn('playing').invokeCallback((_, callback) => {
     const moveLeft = () => callback('MOVE_LEFT');
     const moveRight = () => callback('MOVE_RIGHT');
     const moveDown = () => callback('MOVE_DOWN');
     const rotate = () => callback('ROTATE_PIECE');
+    const hardDrop = () => callback('HARD_DROP');
     const keyDownListener = (e: KeyboardEvent) => {
-        if (e.key === 'ArrowLeft') {
+        if (e.code === 'ArrowLeft') {
             moveLeft()
         }
-        if (e.key === 'ArrowRight') {
+        if (e.code === 'ArrowRight') {
             moveRight()
         }
-        if (e.key === 'ArrowDown') {
+        if (e.code === 'ArrowDown') {
             moveDown()
         }
-        if (e.key === 'ArrowUp') {
+        if (e.code === 'ArrowUp') {
             rotate()
         }
+        if (e.code === 'Space') {
+            hardDrop()
+        }
     }
+    const keyUpListener = () => callback('DECELERATE')
     document.body.addEventListener('keydown', keyDownListener)
+    document.body.addEventListener('keyup', keyUpListener)
     return () => {
         document.body.removeEventListener('keydown', keyDownListener)
+        document.body.removeEventListener('keyup', keyUpListener)
     }
 })
 
-whenIn('playing').on('MOVE_LEFT').moveTo('checkForCollision').updateContext({
-    futurePosition: context => [context.position[0] - 1, context.position[1]],
-    direction: 'left'
-})
+whenIn('playing').on('DECELERATE')
+    .updateContext({
+        shouldAccelerate: false,
+        direction: 'setByDefault'
+    })
 
-whenIn('playing').on('MOVE_RIGHT').moveTo('checkForCollision').updateContext({
-    futurePosition: context => [context.position[0] + 1, context.position[1]],
-    direction: 'right'
-})
+whenIn('playing').on('MOVE_LEFT')
+    .moveTo('checkForCollision')
+    .updateContext({
+        futurePosition: context => [context.position[0] - 1, context.position[1]],
+        direction: 'left',
+        shouldAccelerate: true
+    })
 
-whenIn('playing').on('MOVE_DOWN').moveTo('checkForCollision').updateContext({
-    futurePosition: context => [context.position[0], context.position[1] + 1],
-    direction: 'bottom'
-})
+whenIn('playing').on('MOVE_RIGHT')
+    .moveTo('checkForCollision')
+    .updateContext({
+        futurePosition: context => [context.position[0] + 1, context.position[1]],
+        direction: 'right',
+        shouldAccelerate: true
+    })
+
+whenIn('playing').on('MOVE_DOWN')
+    .moveTo('checkForCollision')
+    .updateContext({
+        futurePosition: context => [context.position[0], context.position[1] + 1],
+        direction: 'bottom',
+        shouldAccelerate: true
+    })
+
+whenIn('playing').on('MOVE_DOWN_FROM_ENGINE')
+    .moveTo('checkForCollision')
+    .updateContext({
+        futurePosition: context => [context.position[0], context.position[1] + 1],
+        direction: 'setByDefault',
+    })
+
+whenIn('playing').on('HARD_DROP')
+    .moveTo('checkForCollision')
+    .updateContext({
+        futurePosition: returnFuturePositionOnHardDrop,
+        direction: 'bottom',
+        shouldAccelerate: false
+    })
+
+
+whenIn('playing')
+    .on('ROTATE_PIECE')
+    .moveTo('checkForCollision')
+    .updateContext({
+        futurePieceAfterRotation: (context) => context.piece === pieces['OPiece'] ? context.piece : rotatePiece(context.piece),
+    })
+
+whenIn('gameCompleteAnimation').after(context => (context.gameDims.rows * 100) + 1000)
+    .moveTo('idle')
